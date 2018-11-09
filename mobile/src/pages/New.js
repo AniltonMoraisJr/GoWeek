4ba3501a6d4136ea6dbe0e2935795870a17dd6f8
Anilton Francisco de Morais Jr <anilton.francisco@gmail.com>
2018-11-09 17:11:55 -0200

    Finishing the mobile project

---
+++ mobile/src/pages/New.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+
+import { StyleSheet, 
+        Text, 
+        TouchableOpacity,
+        View,
+        SafeAreaView,
+        TextInput,
+        AsyncStorage
+} from 'react-native';
+/**
+ * Icon
+ */
+import Icon from 'react-native-vector-icons/MaterialIcons';
+/**
+ * API
+ */
+import api from '../services/api';
+
+export default class New extends Component {
+  static navigationOptions = {
+    header: null 
+  }
+
+  state = {
+    newTweet: ''
+  }
+  goBack = () => {
+    this.props.navigation.pop();
+  };
+
+  handleNewTweet = async () => {
+      const content = this.state.newTweet;
+      const author = await AsyncStorage.getItem("@GoTwitter:username");
+
+      await api.post("tweets", { content, author });
+
+      this.goBack();
+  }
+
+  handleInputChange = newTweet => {
+    this.setState({
+      newTweet
+    });
+  }
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={this.goBack}>
+            <Icon name="close" size={24} color="#4EE0BB" />            
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.handleNewTweet}>
+            <Text style={styles.buttonText}>Tweetar</Text>
+          </TouchableOpacity>          
+        </View>
+        <View>
+          <TextInput style={styles.input}
+                multiline
+                value={this.state.newTweet}
+                placeholder="O que está acontecendo ?"
+                onChangeText={this.handleInputChange}
+                placeholderTextColor="#999"
+                returnKeyType="send"
+                onSubmitEditing={this.handleNewTweet}/>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#FFF"
+    },
+  
+    header: {
+      paddingTop: 10,
+      paddingHorizontal: 20,
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center"
+    },
+  
+    button: {
+      height: 32,
+      paddingHorizontal: 20,
+      borderRadius: 16,
+      backgroundColor: "#4BB0EE",
+      justifyContent: "center",
+      alignItems: "center"
+    },
+  
+    buttonText: {
+      color: "#FFF",
+      fontSize: 16,
+      fontWeight: "bold"
+    },
+  
+    input: {
+      margin: 20,
+      fontSize: 16,
+      color: "#333"
+    }
+  });
+  
